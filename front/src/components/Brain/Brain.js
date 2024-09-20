24062f80989f6a31b4ee0ca7aa68bd81c969803e
andrewmiculka <andrewmiculka@gmail.com>
2024-09-19 21:51:24 -0500

    highlight and cursor change on rollover

--- front/src/components/Brain/Brain.js
+++ front/src/components/Brain/Brain.js
@@ -1,6 +1,7 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
+import { Select, Selection, EffectComposer, Outline } from '@react-three/postprocessing';
 import brainModel from '../../assets/models/me.glb';
 import './Brain.scss';
 
@@ -14,61 +15,67 @@ export function BrainModel(props) {
         groupRef.current.translateY(Math.sin(clock.elapsedTime)/10);
     })
 
+    useEffect(() => {
+        document.body.style.cursor = hovered ? 'pointer' : 'default';
+    }, [hovered]);
+
     const hello = () => {
         console.log('Hello :)');
     }
 
     return (
-        <group
-            {...props}
-            ref={groupRef}
-            scale={10}
-            dispose={null}
-            onClick={hello}
-            onPointerOver={() => setHover(true)}
-            onPointerOut={() => setHover(false)}
-        >
-            <group position={[0.498, 0.652, 3.041]} rotation={[-Math.PI / 2, 0, 0]} scale={4.201}>
-                <group rotation={[Math.PI / 2, 0, 0]}>
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_1.geometry}
-                        material={materials['OrangOetan__LOWOog_binnen.001']}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_2.geometry}
-                        material={materials['Material.003']}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_3.geometry}
-                        material={materials.aiStandardSurface1}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_4.geometry}
-                        material={materials.OrangOetan__LOWOog_binnen}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_5.geometry}
-                        material={materials.OrangOetan__LOWOog_buiten}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_6.geometry}
-                        material={materials['OrangOetan__LOWOog_buiten.001']}
-                    />
+        <Select enabled={hovered}>
+            <group
+                {...props}
+                ref={groupRef}
+                scale={10}
+                dispose={null}
+                onClick={hello}
+                onPointerOver={() => setHover(true)}
+                onPointerOut={() => setHover(false)}
+            >
+                <group position={[0.498, 0.652, 3.041]} rotation={[-Math.PI / 2, 0, 0]} scale={4.201}>
+                    <group rotation={[Math.PI / 2, 0, 0]}>
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_1.geometry}
+                            material={materials['OrangOetan__LOWOog_binnen.001']}
+                        />
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_2.geometry}
+                            material={materials['Material.003']}
+                        />
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_3.geometry}
+                            material={materials.aiStandardSurface1}
+                        />
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_4.geometry}
+                            material={materials.OrangOetan__LOWOog_binnen}
+                        />
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_5.geometry}
+                            material={materials.OrangOetan__LOWOog_buiten}
+                        />
+                        <mesh
+                            castShadow
+                            receiveShadow
+                            geometry={nodes.polySurface7_OrangOetan__LOWOog_binnen_0001_6.geometry}
+                            material={materials['OrangOetan__LOWOog_buiten.001']}
+                        />
+                    </group>
                 </group>
             </group>
-        </group>
+        </Select>
     )
 }
 
@@ -80,7 +87,12 @@ export default function Brain() {
             <ambientLight intensity={Math.PI / 2} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
             <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-            <BrainModel />
+            <Selection>
+                <EffectComposer multisampling={8} autoClear={false}>
+                    <Outline blur visibleEdgeColor="red" edgeStrength={100} width={1000} />
+                </EffectComposer>
+                <BrainModel />
+            </Selection>
         </Canvas>
     )
 }
